cd8682d1a07109c219bb37d4018a65c3b424160d
aida curso <aidacurso@hotmail.com>
2021-12-02 22:15:24 -0300

    attual

--- estdados1211/Program.cs
+++ estdados1211/Program.cs
@@ -5,10 +5,14 @@ namespace estdados1211
 {
     public class Program
     {
-        public static int menu = 0;
-        public static List<string> palavras = new List<string>();
-        public static int QUANT = 0;
-        static List<string> letras = new List<string>();
+        public struct dados
+        {
+            public static int pontuacao = 0;
+            public static bool m = true; /*ativa o switch*/
+            public static int menu = 0; /*variavel reponsavel pelo switcj*/
+            public static List<int> QUANT = new List<int>(); /*quantidade de letras que tem na palavra*/
+            public static List<string> palavras = new List<string>(); /*lista de palavras que foram usadas como palpite*/
+        }
         public static void MENU()
         {
             //menu
@@ -17,48 +21,99 @@ namespace estdados1211
             Console.WriteLine("digite 3 para ver maior pontuação");
             Console.WriteLine("digite 4 para conhecer a desenvolvedora do jogo");
             Console.WriteLine("digite 9 para sair do jogo");
-            menu = int.Parse(Console.ReadLine());
+            dados.menu = int.Parse(Console.ReadLine());
+            /*mostra o menu e pede o numero pra usar no switch e escolher a opção*/
         }
         public static void OP1()
         {
             Console.Clear();
-            Console.WriteLine("DIGITE A QUANTIDADE DE LETRAS QUE VOCÊ IRÁ UTILIZAR NA FORCA.");
-            QUANT = int.Parse(Console.ReadLine());
 
             Console.WriteLine("DIGITE A PALAVRA PARA UTILIZAR NA FORCA");
-            palavras.Add(Console.ReadLine());
-            
+            string cu = Console.ReadLine();
+            dados.palavras.Add(cu);
+            dados.QUANT.Add(cu.Length);
+            /*pede a palavra a ser utilizada*/
+
         }
         public static void OP2()
         {
-            Console.WriteLine("digite uma letra");
-            string letra = Console.ReadLine();
-            for (int i = 0; i < QUANT; i++)
+            List<string> letras = new List<string>();
+            List<string> acertou = new List<string>();
+            string letra = "";
+            int vidas = 5;
+            int numacer = 0;
+            int jota = -1;
+            Random r = new Random();
+            int rand = r.Next(dados.palavras.Count);
+            for (int i = 0; i < dados.QUANT[rand]; i++)
             {
-                for (int j = 0; j < palavras.Count; j++)
-                    letras.Add(palavras[j].Substring(i, 1));
+                    letras.Add(dados.palavras[rand].Substring(i, 1));
             }
-            foreach (string c in letras)
+            /*adiciona as letras da palavra dentro de uma lista*/
+            while (vidas > 0 && numacer != dados.palavras[rand].Length)
             {
-
-                Console.Clear();
-
-                Console.WriteLine("_________");
-                Console.WriteLine("|       |");
-                Console.WriteLine("|");
-                Console.WriteLine("|");
-                Console.WriteLine("|");
-                Console.WriteLine("|");
-                if (letra != c)
+                FORCA();
+            
+                foreach (string c in letras)
                 {
-                    Console.Write(" ___ ");
-                }
-                if (letra == c)
-                {
+
+                    if (letra != c && !acertou.Contains(c))
+                    {
+                    Console.Write(" ___ ");                    
+                    }
+                    /*se a letra palpite estiver errada ele coloca um espaço no lufgar*/
+                    else if (letra == c)
+                    {
                     Console.Write($" {c} ");
+                        acertou.Add(c);
+                        numacer++;
+                    }
+                    else if(acertou.Contains(c)) 
+                    {
+                        Console.Write($" {c} ");
+                    }
+                    /*se a letra palpite estiver certa ele coloca a letra no local*/
                 }
+                if(jota == numacer)
+                {
+                    Console.WriteLine("letra errada");
+                    vidas--;
+                }
+                else
+                {
+                    jota = numacer;
+                }
+                Console.WriteLine($"você ainda tem {vidas} vidas restantes");
+                if (numacer == dados.palavras[rand].Length)
+                {
+                    Console.WriteLine("parabens voce ganhou, pressione enter para continuar");
+                }else if (vidas == 0)
+                {
+                    Console.WriteLine("sinto muito, voce perdeu, pressione enter para continuar");
+                }
+                else
+                {
+                    Console.WriteLine("digite uma letra");
+                }
+                letra = Console.ReadLine();
+                Console.Clear();
+                /*palpite*/
             }
-
+            
+            if (vidas > dados.pontuacao)
+            {
+                dados.pontuacao = vidas;
+            }
+            
+        }
+        public static void OP4()
+        {
+            Console.Clear();
+            Console.WriteLine("Esse jogo foi desenvolvido por Aída, aluna do segundo módulo do curso técnico de informática do CEET Vasco Coutinho");
+        }
+        public static void OP3()
+        {
+            Console.WriteLine($"a maior pontuacao foi: {dados.pontuacao}");
         }
         public static void FORCA()
         {
@@ -68,31 +123,31 @@ namespace estdados1211
             Console.WriteLine("|");
             Console.WriteLine("|");
             Console.WriteLine("|");
-            for (int i = 0; i < QUANT; i++)
-            {
-                Console.Write(" ___ ");
-            }
+            //for (int i = 0; i < dados.QUANT; i++)
+            //{
+            //    Console.Write(" ___ ");
+            //}
         }
         
 
          static void Main(string[] args)
          {
-            bool m = true;
             
-            while (m) {
+            
+            while (dados.m) {
                 MENU();
-                switch (menu)
+                switch (dados.menu)
                 {
                     case 1: OP1();
                             break;
                         
                     case 2: OP2();
                             break;
-                        
-                    //case 3 OP3();
-                    //    break;
-                    //case 4: OP4();
-                    //    break;
+
+                    case 3: OP3();
+                            break;
+                    case 4: OP4();
+                           break;
                     case 9: return;
 
 
